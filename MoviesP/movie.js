1b1240f38259a987a4d65b8b4c981c9defffcc64
Matan Rapheal Yaish <113148483+MatanYaish@users.noreply.github.com>
2023-05-14 01:08:10 +0300

    Update movie.js
    Overall, building MoveList was a great way to practice my JavaScript, HTML, CSS, MySQL, and Bootstrap skills. By building a web application with dynamic functionality for searching movies names, as well as persisting the data in a database, I was able to gain a better understanding of these technologies and how they can be used together to create interactive web applications.

--- MoviesP/movie.js
+++ MoviesP/movie.js
@@ -1,7 +1,6 @@
 var base = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?';
 var apikey = 'JtoqAANtDZP1xsaRtcBNlAKJ3BAZbPQF';
 
-//console.log(base + "api-key=" + apikey + "&query=" + "rambo");
 
 $(document).ready(function () {
   $('.btn').click(function () {
@@ -17,15 +16,8 @@ $(document).ready(function () {
           query: $('.input').val(),
         },
         url: base,
-        success: function (data) {
-          //console.log(data.results[0].display_title);
-          //$("p").text(data.results[0].display_title);
-
-          // for(var i = 0; i < data.results.length; i++){
-          //     var newName = $("<p>" + data.results[i].display_title + "</p>");
-          //     $(".container").append(newName);
-          // }
-
+        success: function (data) 
+        
           for (var i = 0; i < data.results.length; i++) {
             console.log(
               "<a href='" +
@@ -43,13 +35,6 @@ $(document).ready(function () {
             );
             $('.container').append(newName);
           }
-
-          // for(var i = 0; i < data.results.length; i++){
-          //     if(data.results[i].multimedia != null){
-          //         var newImg = $("<img src='" + data.results[i].multimedia.src  + "'><br><br>");
-          //         $(".container").append(newImg);
-          //     }
-          // }
         },
       });
     }
